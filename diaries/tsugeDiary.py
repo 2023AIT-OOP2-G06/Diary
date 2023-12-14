75031818639258bc3a56ea3215b80c587f9a7f56
tsugeyuto <k22090kk@aitech.ac.jp>
2023-12-14 15:44:09 +0900

    a

---
+++ diaries/tsugeDiary.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class tsugeDiary(AbstractDiary):
+    
+    def get_date(self):
+        return "2021-12-14"
+    
+    def get_summary(self):
+        return "なにもない一日だった"
+    
+    def get_author(self):
+        return "柘植"
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.tsugeDiary import tsugeDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ]
+diaries = [DiarySample(),
+           tsugeDiary(), ]
+
 
 for d in diaries:
     print("---------------------------------")
