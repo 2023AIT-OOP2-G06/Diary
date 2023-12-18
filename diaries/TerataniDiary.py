9fe8cd72fc0cc7804e93ce51abe2a22e2fb532a7
teratanitaiki <153792045+teratanitaiki@users.noreply.github.com>
2023-12-18 15:21:53 +0900

    寺谷太樹日記作成

--- diaries/TerataniDiary.py
+++ diaries/TerataniDiary.py
@@ -1,12 +1,12 @@
 from diaries.AbstractDiary import AbstractDiary
 
-class DiarySample(AbstractDiary):
+class TerataniDiary(AbstractDiary):
     
     def get_date(self):
-        return "2021-12-01"
+        return "2021-12-15"
     
     def get_summary(self):
-        return "なにもない一日だった"
+        return "なにもない一日じゃなかった"
     
     def get_author(self):
         return "Teratani"
